0dcdc1fab135e77b1eb8efbe33a4e258b39bab1e
kiyeun-kim <kiyeun.kim@growdle.com>
2025-04-23 21:05:51 +0900

    etc: util 파일명 변경

--- src/shared/ui/Calendar.tsx
+++ src/shared/ui/Calendar.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 import { DayPicker } from 'react-day-picker';
 
 import { cn } from '@/shared/lib/utils';
-import { buttonVariants } from '@/shared/ui/button';
+import { buttonVariants } from '@/shared/ui/Button';
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
