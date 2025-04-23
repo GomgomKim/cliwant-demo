a9714a9875173f8a5b52fe0d0a050384eb33f318
kiyeun-kim <kiyeun.kim@growdle.com>
2025-04-23 21:03:40 +0900

    etc: utils 파일명 변경

--- src/shared/ui/DatePicker.tsx
+++ src/shared/ui/DatePicker.tsx
@@ -5,7 +5,7 @@ import { Calendar as CalendarIcon } from 'lucide-react';
 import * as React from 'react';
 
 import { cn } from '@/shared/lib/utils';
-import { Button } from '@/shared/ui/button';
+import { Button } from '@/shared/ui/Button';
 import { Calendar } from '@/shared/ui/calendar';
 import { Popover, PopoverContent, PopoverTrigger } from '@/shared/ui/popover';
 
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,3 @@
-export { Button, buttonVariants } from './button';
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './card';
+export { Button, buttonVariants } from './Button';
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './Card';
 export { ThemeProvider } from './ThemeProvider';
