ed68de0fe05a6cd334041392b7d9ad81416f3e6f
kiyeun-kim <kiyeun.kim@growdle.com>
2025-04-28 00:50:32 +0900

    feat: 홈페이지 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { HomeWidget } from '@/widgets/HomeWidget';
+import { BidSearchView } from '@/views/bid-search/BidSearchView';
 
 export default function Home() {
   return (
     <div className="container mx-auto px-4 py-8">
-      <HomeWidget />
+      <BidSearchView />
     </div>
   );
 }
